9a7ca4ee8f5c7e4e18af08f3f8c64e1142a5ca65
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-28 18:05:35 +0100

    Create class for questions. Import country flags.

--- app/src/main/java/com/example/quizappxml/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappxml/QuizQuestionsActivity.kt
@@ -2,10 +2,13 @@ package com.example.quizappxml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+        val questionsList =Constants.getQuestions()
+        Log.i("questionList size is","${questionsList.size}")
     }
 }
