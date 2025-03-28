8d8509e439fb1a90da3cbe99869f4cd8baae1b00
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-28 14:29:57 +0100

    Add actitivity for questions.

--- app/src/main/java/com/example/quizappxml/MainActivity.kt
+++ app/src/main/java/com/example/quizappxml/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.quizappxml
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_start:Button = findViewById(R.id.btn_start);
+        val name_filed:EditText = findViewById(R.id.name_field);
+
+        btn_start.setOnClickListener {
+            if(name_filed.text.isEmpty())
+            {
+                Toast.makeText(this,"Enter name",Toast.LENGTH_SHORT).show()
+
+            }
+            else
+            {
+                val intent:Intent = Intent(this,QuizQuestionsActivity::class.java);
+                startActivity(intent);
+                finish(); // closes current activity
+            }
+        }
     }
 }
